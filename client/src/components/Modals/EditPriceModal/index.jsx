4fb9a0257a57a2c888ba19aa114d8de4c1c47e1e
AnneLiseDesolOclock <anne-lise.desol@oclock.school>
2024-06-05 16:53:59 +0200

    style: add css editpricemodal

--- client/src/components/Modals/EditPriceModal/index.jsx
+++ client/src/components/Modals/EditPriceModal/index.jsx
@@ -1,7 +1,6 @@
-import Modal from 'react-modal';
 import { useState, useEffect } from 'react';
 import API_URL from '../../../config.js';
-import { Alert } from "@mui/material";
+import { Modal, Box, Typography, TextField, Button, Alert, FormControl, InputLabel, Select, MenuItem } from '@mui/material';
 
 const EditPriceModal = ({ priceId, isOpen, onRequestClose, initialValues, onClose }) => {
 
@@ -51,47 +50,63 @@ const EditPriceModal = ({ priceId, isOpen, onRequestClose, initialValues, onClos
 
     return (
         <Modal
-            isOpen={isOpen}
-            onRequestClose={onRequestClose}
+            open={isOpen}
+            onClose={onRequestClose}
+            style={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center'
+            }}
         >
-            <form method="post" onSubmit={handleSubmit}>
-                {successMessage && <Alert variant='filled' severity='success'>{successMessage}</Alert>}
-                {errorMessage && <Alert variant='filled' severity='error'>{errorMessage}</Alert>}
-                <p>* Champs obligatoires</p>
-                <div>
-                    <label htmlFor="duration">Durée (en jours) *</label>
-                    <input
-                        type="number"
+            <Box sx={{ width: 400, bgcolor: 'background.paper', boxShadow: 24, p: 4 }}>
+                <Typography sx={{ color: 'black' }} gutterBottom>
+                    Modifier un prix
+                </Typography>
+                <form onSubmit={handleSubmit}>
+                    {successMessage && <Alert sx={{ my: '1rem' }} variant='filled' severity='success'>{successMessage}</Alert>}
+                    {errorMessage && <Alert sx={{ my: '1rem' }} variant='filled' severity='error'>{errorMessage}</Alert>}
+                    <Typography sx={{ color: 'gray', mb: 2 }}>Tous les champs sont obligatoires.</Typography>
+                    <TextField
+                        sx={{ mb: 2 }}
+                        fullWidth
+                        label="Durée (en jours)"
                         name="duration"
-                        id="duration"
+                        type='number'
                         value={formData.duration}
                         onChange={handleChange}
                         required
                     />
-                </div>
-                <div>
-                    <label htmlFor="price">Montant (au format XX.XX)*</label>
-                    <input
-                        type="text"
-                        // Pattern autorisant uniquement les chiffres et 2 chiffres après la virgule
+                    <TextField
+                        sx={{ mb: 2 }}
+                        fullWidth
+                        label="Montant (au format XX.XX)"
+                        name="price"
+                        type='text'
                         pattern="\d+(\.\d{2})?"
                         title="Le montant doit être au format XX.XX"
-                        name="price"
-                        id="price"
                         value={formData.price}
                         onChange={handleChange}
                         required
                     />
-                </div>
-                <div>
-                    <label htmlFor="hotel">Hôtel inclus ? *</label>
-                    <select name="hotel" id="hotel" required onChange={handleChange} value={formData.hotel}>
-                        <option value="true">Oui</option>
-                        <option value="false">Non</option>
-                    </select>
-                </div>
-                <button type="submit">Modifier les informations</button>
-            </form>
+                    <FormControl fullWidth variant='filled'>
+                        <InputLabel>Hôtel inclus ?</InputLabel>
+                        <Select
+                            name="hotel"
+                            required
+                            onChange={handleChange} 
+                            value={formData.hotel}
+                        >
+                            <MenuItem sx={{ color: 'black' }} value="true">Oui</MenuItem>
+                            <MenuItem sx={{ color: 'black' }} value="false">Non</MenuItem>
+                        </Select>
+                    </FormControl>
+                    <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+                        <Button type="submit" variant="contained" sx={{ mt: 2 }}>
+                            Modifier le prix
+                        </Button>
+                    </Box>
+                </form>
+            </Box>
         </Modal>
     )
 }
