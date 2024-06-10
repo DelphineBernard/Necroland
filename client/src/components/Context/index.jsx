70cf88ccea8709c141e574b400d3bd1e0c60cacb
Delphine Bernard <144319314+DelphineBernard@users.noreply.github.com>
2024-06-10 15:29:55 +0200

    Revert "Feature front route attractions infos pratiques plan du site style"

--- client/src/components/Context/index.jsx
+++ client/src/components/Context/index.jsx
@@ -2,14 +2,15 @@ import React, { createContext, useState, useEffect } from 'react';
 import API_URL from '../../config.js';
 
 const Context = createContext();
-
+// Le Context va permettre de rendre disponibles tous les states initialisés ici à tous les composants enfants
+// Pour se faire, dans le fichier index.js on enveloppe le router dans la balise ContextProvider
 const ContextProvider = ({ children }) => {
+
     const [attractions, setAttractions] = useState([]);
     const [categories, setCategories] = useState([]);
     const [tags, setTags] = useState([]);
     const [tagSearched, setTagSearched] = useState("");
     const [allPrices, setAllPrices] = useState([]);
-    const [category, setCategory] = useState("all"); // New state for category
 
     const fetchCategories = async () => {
         try {
@@ -27,51 +28,16 @@ const ContextProvider = ({ children }) => {
         setAllPrices(data.prices);
     };
 
-    const fetchAttractions = async (category) => {
-        try {
-            let response;
-            if (category === "all" || !category) {
-                response = await fetch(`${API_URL}/attractions`);
-            } else {
-                response = await fetch(`${API_URL}/attractions/${category}`);
-            }
-            const data = await response.json();
-            setAttractions(data.attractions);
-        } catch (error) {
-            console.log(error);
-        }
-    };
-
-    const resetCategory = () => {
-        setCategory("all");
-        fetchAttractions("all");
-    };
-
     useEffect(() => {
         fetchCategories();
         fetchPrices();
-        fetchAttractions("all");
     }, []);
 
     return (
-        <Context.Provider value={{
-            attractions,
-            setAttractions,
-            categories,
-            setCategories,
-            tags,
-            setTags,
-            tagSearched,
-            setTagSearched,
-            allPrices,
-            setAllPrices,
-            category,
-            setCategory,
-            resetCategory // Provide resetCategory function
-        }}>
+        <Context.Provider value={{attractions, setAttractions, categories, setCategories, tags, setTags, tagSearched, setTagSearched, allPrices, setAllPrices}}>
             {children}
         </Context.Provider>
-    );
+    )
 }
 
 export { Context, ContextProvider };
