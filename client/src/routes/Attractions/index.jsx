fe1e98ee2dc4dc5c62b8958d45174c29cff0be50
salma13012016 <salma.belhsen@oclock.school>
2024-06-11 15:19:54 +0200

    style: add css attractions infos pratiques pages

--- client/src/routes/Attractions/index.jsx
+++ client/src/routes/Attractions/index.jsx
@@ -1,11 +1,13 @@
-import React, { useContext, useEffect, useRef, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { useParams } from "react-router-dom";
-import SwiperCore, { Autoplay, Navigation, Pagination } from 'swiper';
+import SwiperCore, { Autoplay, Navigation, Pagination, EffectCoverflow } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/swiper-bundle.min.css';
 import 'swiper/swiper.min.css';
 import 'swiper/components/navigation/navigation.min.css';
 import 'swiper/components/pagination/pagination.min.css';
+import 'swiper/components/effect-coverflow/effect-coverflow.min.css'; // Correct import path for effect-coverflow
+import { styled } from '@mui/system';
 import Alerte from "../../assets/icons/alerte.png";
 import Card from "../../components/Card";
 import SearchForm from "../../components/SearchForm";
@@ -13,16 +15,36 @@ import CategoryTabs from "../../components/CategoryTabs";
 import { Context } from "../../components/Context";
 import API_URL from '../../config.js';
 
-
 // Initialize Swiper modules
-SwiperCore.use([Autoplay, Navigation, Pagination]);
-
+SwiperCore.use([Autoplay, Navigation, Pagination, EffectCoverflow]);
+const AlertSection = styled('div')({
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    textAlign: 'center',
+    padding: '10px',
+    marginBottom: '30px',
+    '@media (min-width: 640px)': {
+        flexDirection: 'row',
+    },
+});
+const CenteredHeading = styled('h2')(({ theme }) => ({
+    textAlign: 'center',
+    margin: '16px 0',
+    paddingTop: '20px',
+    fontFamily: 'Cinzel, serif',
+    fontWeight: 'bold',
+    fontSize: '1.5rem',
+    [theme.breakpoints.up('md')]: {
+      fontSize: '2rem',
+    },
+  }));
 const Attractions = () => {
     const { attractions, setAttractions } = useContext(Context);
-    const { category } = useParams();// Récupère le paramètre de catégorie de l'URL
+    const { category } = useParams(); // Récupère le paramètre de catégorie de l'URL
     const [activeSlide, setActiveSlide] = useState(null);
     const swiperRef = useRef(null);
-
     const fetchAttractions = async () => {
         try {
             let response;
@@ -35,11 +57,9 @@ const Attractions = () => {
             console.log(error);
         }
     };
-
     useEffect(() => {
         fetchAttractions();
-    }, [category]);// Re-fetch les attractions chaque fois que la catégorie change
-
+    }, [category]); // Re-fetch les attractions chaque fois que la catégorie change
     const handleSlideClick = (index) => {
         if (index === activeSlide) {
             setActiveSlide(null);
@@ -49,31 +69,42 @@ const Attractions = () => {
             swiperRef.current.swiper.autoplay.stop();
         }
     };
-
+    const middleSlideIndex = Math.floor(attractions.length / 2); // Dynamically calculate the middle slide index
     return (
         <main>
-            <div>
+            <AlertSection>
                 <img src={Alerte} alt="Alerte" />
                 <p>Le parc est interdit au moins de 16 ans</p>
-            </div>
-            <div>
+            </AlertSection>
+            <div className="Filter">
                 <CategoryTabs />
                 <SearchForm />
             </div>
             <section>
-                <h2>Des attractions à couper le souffle</h2>
+                <CenteredHeading>Des attractions à couper le souffle</CenteredHeading>
         
                 <Swiper
                     ref={swiperRef}
-                    spaceBetween={30}
-                    slidesPerView={3}
+                    effect="coverflow"
+                    grabCursor={true}
+                    centeredSlides={true}
+                    slidesPerView="auto"
+                    initialSlide={middleSlideIndex}
+                    loop={true}
+                    coverflowEffect={{
+                        rotate: 50,
+                        stretch: 0,
+                        depth: 100,
+                        modifier: 1,
+                        slideShadows: true,
+                    }}
                     autoplay={{
                         delay: 3000,
                         disableOnInteraction: false,
                     }}
                     navigation
                     pagination={{ clickable: true }}
-                    className="mySwiper"  // Ajoutez cette classe
+                    className="mySwiper"
                     breakpoints={{
                         320: {
                             slidesPerView: 1, // Mobile
@@ -81,11 +112,11 @@ const Attractions = () => {
                         },
                         640: {
                             slidesPerView: 2, // Tablette
-                            spaceBetween: 20,
+                            spaceBetween: 30,
                         },
                         1024: {
                             slidesPerView: 3, // Desktop
-                            spaceBetween: 30,
+                            spaceBetween: 40,
                         },
                     }}
                 >
@@ -110,5 +141,4 @@ const Attractions = () => {
         </main>
     );
 }
-
 export default Attractions;
