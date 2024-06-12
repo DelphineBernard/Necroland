a0133754bd276eb6ed4f7f0c496a89c86b196061
salma13012016 <salma.belhsen@oclock.school>
2024-06-12 19:41:25 +0200

    added icons material ui

--- client/src/routes/InfosPratiques/index.jsx
+++ client/src/routes/InfosPratiques/index.jsx
@@ -6,11 +6,11 @@ import { Typography, Box, Button, Grid, Paper, Table, TableBody, TableCell, Tabl
 import { styled } from '@mui/system';
 import { red } from '@mui/material/colors';
 import Alerte from "../../assets/icons/alerte.png";
-import autobus from "../../assets/icons/autobus.png";
-import train from "../../assets/icons/train.png";
-import voiture from "../../assets/icons/voiture.png";
-import fleche from "../../assets/icons/fleche.png";
-import telephone from "../../assets/icons/telephone.png";
+import DirectionsCarFilledRoundedIcon from '@mui/icons-material/DirectionsCarFilledRounded';
+import DirectionsBusRoundedIcon from '@mui/icons-material/DirectionsBusRounded';
+import TrainRoundedIcon from '@mui/icons-material/TrainRounded';
+import ChevronRightOutlinedIcon from '@mui/icons-material/ChevronRightOutlined';
+import LocalPhoneOutlinedIcon from '@mui/icons-material/LocalPhoneOutlined';
 import rollercoaster from "../../assets/img/rollercoaster.webp";
 import { Icon } from "leaflet";
 import logo from "../../assets/img/logo.webp";
@@ -24,6 +24,7 @@ import 'swiper/components/pagination/pagination.min.css';
 
 
 SwiperCore.use([Pagination]);
+
 const AlertSection = styled('div')({
   display: 'flex',
   flexDirection: 'column',
@@ -119,9 +120,13 @@ const ResponsiveBox = styled(Box)(({ theme }) => ({
   hyphens: 'auto', // Enable hyphenation
   wordBreak: 'break-word', // Allow words to break
   overflowWrap: 'break-word', // Ensure words break to avoid overflow
+  lineHeight: '1.6', // Ajout de l'espacement entre les lignes
+  letterSpacing: '0.5px', // Ajout de l'espacement entre les lettres
   [theme.breakpoints.up('sm')]: {
     width: '60%', // Ajustement pour les tablettes
     padding: '16px', // Ajustement pour les tablettes
+    lineHeight: '1.2', // Ajout de l'espacement entre les lignes
+    letterSpacing: '0.2px', // Ajout de l'espacement entre les lettres
   },
   [theme.breakpoints.up('md')]: {
     width: '40%', // Réduit la largeur pour les écrans moyens et plus grands
@@ -132,7 +137,6 @@ const ResponsiveBox = styled(Box)(({ theme }) => ({
     marginRight: '2px'
   },
 }));
-
 const MapContainerWrapper = styled(Box)(({ theme }) => ({
   display: 'flex',
   justifyContent: 'center',
@@ -160,34 +164,11 @@ const ContactContainer = styled(Box)(({ theme }) => ({
   },
 }));
 const ContactInfo = styled('a')(({ theme }) => ({
-  color: 'inherit',
   textDecoration: 'none',
   fontSize: '1rem',
   marginLeft: theme.spacing(2),
   [theme.breakpoints.up('sm')]: {
     fontSize: '1.2rem',
-  },
-}));
-const PhoneIconContainer = styled(Box)(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  width: '50px',
-  height: '50px',
-  borderRadius: '20px',
-  backgroundColor: '#d9d9d9',
-  [theme.breakpoints.down('sm')]: {
-    width: '30px',
-    height: '30px',
-    borderRadius: '15px',
-  },
-}));
-const PhoneIconStyled = styled('img')(({ theme }) => ({
-  width: '30px',
-  height: '30px',
-  [theme.breakpoints.down('sm')]: {
-    width: '20px',
-    height: '20px',
   },
 }));
 const SectionContainer = styled(Box)(({ theme }) => ({
@@ -231,10 +212,9 @@ const IconContainer = styled(Box)({
   backgroundColor: 'var(--dark_gray)',
   margin: '8px',
 });
-const IconStyled = styled('img')({
-  width: '40px',
-  height: '40px',
-});
+const ChevronIcon = styled(ChevronRightOutlinedIcon)(({ size }) => ({
+  fontSize: size || '50px',
+}));
 const TextContainer = styled(Box)(({ theme }) => ({
   display: 'flex',
   alignItems: 'center',
@@ -295,7 +275,6 @@ const PopupContent = styled(Box)(({ theme }) => ({
 const PopupText = styled(Typography)({
   color: 'black',
 });
-
 
 const InfosPratiques = () => {
   const [prices, setPrices] = useState([]);
@@ -490,10 +469,8 @@ const InfosPratiques = () => {
         </MapContainer>
       </MapContainerWrapper>
       <ContactContainer>
-        <PhoneIconContainer>
-          <PhoneIconStyled src={telephone} alt="icone telephone" />
-        </PhoneIconContainer>
-        <ContactInfo href="tel:+33123456789">01 23 45 67 89</ContactInfo>
+          <LocalPhoneOutlinedIcon sx={{color: 'white'}} />
+          <ContactInfo href="tel:+33123456789">01 23 45 67 89</ContactInfo>
       </ContactContainer>
       <SectionContainer>
         <Grid container spacing={4} justifyContent="center">
@@ -503,15 +480,15 @@ const InfosPratiques = () => {
             </Typography>
             <DirectionContainer>
               <IconContainer>
-                <IconStyled src={autobus} alt="icone bus" />
+                <DirectionsBusRoundedIcon style={{ fontSize: 50 }} />
               </IconContainer>
               <TextContainer>
                 <TextWithIcon>
-                  <IconStyled src={fleche} alt="icone flèche de direction" />
+                  <ChevronIcon  />
                   <Typography>De Paris, prendre la ligne de BUS 3202</Typography>
                 </TextWithIcon>
                 <TextWithIcon>
-                  <IconStyled src={fleche} alt="icone flèche de direction" />
+                  <ChevronIcon />
                   <Typography>Arrêt Mairie La Tombe</Typography>
                 </TextWithIcon>
               </TextContainer>
@@ -528,23 +505,23 @@ const InfosPratiques = () => {
             </Typography>
             <DirectionContainer>
               <IconContainer>
-                <IconStyled src={train} alt="icone train" />
+                <TrainRoundedIcon style={{ fontSize: 50 }} />
               </IconContainer>
               <TextContainer>
                 <TextWithIcon>
-                  <IconStyled src={fleche} alt="icone flèche de direction" />
+                  <ChevronIcon />
                   <Typography>De Paris prendre le TER Gare de Lyon</Typography>
                 </TextWithIcon>
                 <TextWithIcon>
-                  <IconStyled src={fleche} alt="icone flèche de direction" />
+                  <ChevronIcon />
                   <Typography>Arrivée Gare SNCF de Montreaul</Typography>
                 </TextWithIcon>
                 <TextWithIcon>
-                  <IconStyled src={fleche} alt="icone flèche de direction" />
+                  <ChevronIcon />
                   <Typography>Prendre le Bus 3202</Typography>
                 </TextWithIcon>
                 <TextWithIcon>
-                  <IconStyled src={fleche} alt="icone flèche de direction" />
+                  <ChevronIcon />
                   <Typography>Arrêt Mairie La Tombe</Typography>
                 </TextWithIcon>
               </TextContainer>
@@ -561,24 +538,20 @@ const InfosPratiques = () => {
             </Typography>
             <DirectionContainer>
               <IconContainer>
-                <IconStyled src={voiture} alt="icone voiture" />
+                <DirectionsCarFilledRoundedIcon style={{ fontSize: 50 }}/>
               </IconContainer>
               <TextContainer>
                 <TextWithIcon>
-                  <IconStyled src={fleche} alt="icone flèche de direction" />
+                  <ChevronIcon />
                   <Typography>De Paris, en direction de A4 à Charenton-le-Pont.</Typography>
                 </TextWithIcon>
                 <TextWithIcon>
-                  <IconStyled src={fleche} alt="icone flèche de direction" />
+                  <ChevronIcon />
                   <Typography>Prendre N104 et A5 en direction de Marolles-sur-Seine. Prendre la sortie 18</Typography>
                 </TextWithIcon>
                 <TextWithIcon>
-                  <IconStyled src={fleche} alt="icone flèche de direction" />
+                  <ChevronIcon />
                   <Typography>Suivre D411 en direction de La Tombe</Typography>
-                </TextWithIcon>
-                <TextWithIcon>
-                  <IconStyled src={fleche} alt="icone flèche de direction" />
-                  <Typography>Entrer dans La tombe.</Typography>
                 </TextWithIcon>
               </TextContainer>
               <ButtonContainer>
